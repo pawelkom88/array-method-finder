dcc7bf56c49997f7f2f41435e1e679801e499ba1
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-06-04 17:11:15 +0100

    Update Main component with necessary logic and sub-components

--- src/components/stateful/Main.js
+++ src/components/stateful/Main.js
@@ -1,6 +1,11 @@
 import {useState} from 'react';
-// import arrayMethods from '../../data/arrayMethods'
+
+import arrayMethods from '../../data/arrayMethods';
 import MethodFinder from './MethodFinder';
+import MethodDesc from './MethodDesc';
+import MethodOutput from './MethodOutput';
+import Usage from './Usage';
+import Output from './Output';
 import AddItems from '../method-types/AddItems';
 import RemoveItems from '../method-types/RemoveItems';
 
@@ -12,16 +17,25 @@ export default function Main() {
 
   return (
     <main className={classes.main}>
-      <MethodFinder setMethod={setMethodType}>
+      <MethodFinder setMethodType={setMethodType}>
         {methodType && (
           <div className="container">
             <div className="method-types">
               {methodType === 'addItems' && <AddItems setChooseMethod={setChooseMethod} />}
               {methodType === 'removeItems' && <RemoveItems setChooseMethod={setChooseMethod} />}
             </div>
+            {chooseMethod && <MethodDesc data={arrayMethods} chooseMethod={chooseMethod} />}
           </div>
         )}
       </MethodFinder>
+      <MethodOutput methodType={methodType} chooseMethod={chooseMethod}>
+        {methodType && chooseMethod && (
+          <>
+            <Usage data={arrayMethods} chooseMethod={chooseMethod} />
+            <Output data={arrayMethods} chooseMethod={chooseMethod} />
+          </>
+        )}
+      </MethodOutput>
     </main>
   );
 }
